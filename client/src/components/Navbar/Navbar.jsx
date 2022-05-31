be0fb5a329933cd9f22e4b21df49c9c0fed13799
David Castellanos <49693134+Davidcastel26@users.noreply.github.com>
2022-05-31 17:55:54 -0600

    update

--- client/src/components/Navbar/Navbar.jsx
+++ client/src/components/Navbar/Navbar.jsx
@@ -1,20 +1,23 @@
-import React from 'react'
-import {AppBar, Typography} from '@material-ui/core'
+import React from "react";
+import { AppBar, Typography } from "@material-ui/core";
 
 //my own
-import memories from '../../images/memories.png';
-import useStyles  from './styles'
+import memories from "../../images/memories.png";
+import useStyles from "./styles";
 
 const Navbar = () => {
-
-  const classes = useStyles()
+  const classes = useStyles();
 
   return (
-    <div className={classes.appBar} position="static" color="inherit">
-        <Typography className={classes.heading} variant="h2" align="center">Memories</Typography>
+    <AppBar className={classes.appBar} position="static" color="inherit">
+      <div className={classes.heading}>
+        <Typography className={classes.heading} variant="h2" align="center">
+          Memories
+        </Typography>
         <img className={classes.image} src={memories} alt="icon" height="60" />
       </div>
-  )
-}
+    </AppBar>
+  );
+};
 
-export default Navbar
+export default Navbar;
