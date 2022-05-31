802ed25f911bafddaa4e17c54acc3fe92bb44717
David Castellanos <49693134+Davidcastel26@users.noreply.github.com>
2022-05-31 17:45:26 -0600

    Update Navbar.jsx

--- client/src/components/Navbar/Navbar.jsx
+++ client/src/components/Navbar/Navbar.jsx
@@ -1,6 +1,10 @@
 import React from 'react'
+import {AppBar, Typography} from '@material-ui/core'
 
 const Navbar = () => {
+
+  const classes = useStyles()
+
   return (
     <div className={classes.appBar} position="static" color="inherit">
         <Typography className={classes.heading} variant="h2" align="center">Memories</Typography>
